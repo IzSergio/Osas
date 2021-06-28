cdc61ee2e16c8bda7b1caa0c1c8c7fc8d5e58d37
IzSergio <83692005+IzSergio@users.noreply.github.com>
2021-06-28 17:20:07 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -98,24 +98,6 @@ def net():
  # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
  # сети если был нажат сабмит, либо передадим falsy значения
  return render_template('net.html',form=form,image_name=filename,neurodic=neurodic)
-    
-#def iz():
- 
-
-import matplotlib.pyplot as plt
-def distr(img, fig, ax, name, f):
-    res = np.array(img.histogram()).reshape(3,256)
-    zer = np.arange(256)
-    ax.fill_between(zer, res[0], alpha=0.4, color='red')
-    ax.fill_between(zer, res[1], alpha=0.4, color='green')
-    ax.fill_between(zer, res[2], alpha=0.4, color='blue')
-    ax.set_xlabel('Color Intensity')
-    ax.set_ylabel('Frequency')
-    ax.set_title(name)
-    fig.savefig(f)
-    plt.cla()
-    return res
- fig, ax = plt.subplots(figsize=(10, 10))
  
 # метод для обработки запроса от пользователя
 @app.route("/apinet",methods=['GET', 'POST'])
